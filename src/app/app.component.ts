7df922e80886fff4b6f0d2cbb82cbf45f3570ec3
somayeh_majidi <somayeh_majidi@outlook.com>
2024-01-15 15:32:33 +0330

    another update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PostsService } from './posts.service';
+import { HttpClient } from '@angular/common/http';
+import { PostModel } from './post.model';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'infinite-scroll';
+
+export class AppComponent implements OnInit {
+  title = 'infinite-scroll'
+
+  public posts: PostModel[] = []
+  private unsubscribe$ = new Subject<void>();
+  
+  constructor (private service: PostsService) {}
+
+  ngOnInit(): void {
+    // this.http.get('https://sam-angular-infinite-scroll-default-rtdb.firebaseio.com/posts.json')
+    // .subscribe(
+    //   res => {
+    //     console.log(res)
+    //   }
+    // )
+    this.getPosts()
+  }
+
+  getPosts() {
+    this.service.posts$
+    .pipe(takeUntil(this.unsubscribe$))
+    .subscribe(
+      v => { this.posts = v }
+    )
+  }
+
+  private unSubscribed() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgxSkeletonLoaderModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
